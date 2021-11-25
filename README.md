f221c3aae4b6c70b2d0d77e87f91877e046fac06
OyinkuroBenafa <81170416+OyinkuroBenafa@users.noreply.github.com>
2021-11-25 13:04:55 +0000

    Initial Commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+
+# VLSI-Fundamentals-A-Practical-Approach-Education-Kit
+
+Welcome to our VLSI Fundamentals: A Practical Approach Education Kit!
+
+### [Download the EdKit here](https://github.com/arm-university/VLSI-Fundamentals-A-Practical-Approach-Education-Kit/archive/refs/heads/main.zip)
+
+Our flagship offering to universities worldwide is the Arm University Program Education Kit series.
+
+These self-contained educational materials are offered exclusively and at no cost to academics and teaching staff worldwide. They’re designed to support your day-to-day teaching on core electronic engineering and computer science subjects. You have the freedom to choose which modules to teach – you can use all the modules in the Education Kit or only those that are most appropriate to your teaching outcomes.
+
+VLSI Fundamentals: A Practical Approach Education Kit covers the fundamentals of Very Large-Scale Integration (VLSI) design, including how the theories and concepts can be applied in the design of simple logic circuits and in the physical implementation of a simplified microprocessor. A full description of the education kit can be found [here](https://www.arm.com/resources/education/education-kits/vlsi-fundamentals-a-practical-approach).
+
+
+ ## Kit specification:
+
+* A full set of 20 modules with lecture slides and lab exercises (in selected modules) ready for use in a typical 10-12-week undergraduate course (full syllabus below).
+* Lab manual with solutions for faculty. 
+* **Level:** Students are required to have an understanding of digital electronics and the basics of hardware description language (Verilog).
+
+## Course Aim
+To produce students with solid introductory knowledge on VLSI concepts and application of these concepts in simulation, verification, and physical implementation of a simplified microprocessor using standard industry tools.
+
+## Tools notice
+Cadence Innovus tool can be used in place of the older tool Cadence SoC Encounter for PnR.
+* **Affected Lab:** Lab 3. 
+
+## Syllabus
+1. Introduction
+1. Circuits and Layout 
+1. Simple Processor Example
+1. CMOS Transistor Theory
+1. Nonideal Transistor Theory
+1. DC & Transient Response
+1. Logical Effort
+1. Power
+1. Scaling
+1. SPICE Simulation
+1. Combinational Circuit Design
+1. Sequential Circuit Design
+1. Wires
+1. Adders
+1. Datapath Functional Units
+1. SRAM
+1. Clocking
+1. Variation and Reliability
+1. Test
+1. Packaging, I/O & Power Distribution
+
+
+
+
+
+
+## License
+You are free to fork or clone this material. See [LICENSE.md](https://github.com/arm-university/Efficient-Embedded-Systems-Design-Education-Kit/blob/main/License/LICENSE.md) for the complete license.
+
+## Inclusive Language Commitment
+Arm is committed to making the language we use inclusive, meaningful, and respectful. Our goal is to remove and replace non-inclusive language from our vocabulary to reflect our values and represent our global ecosystem.
+ 
+Arm is working actively with our partners, standards bodies, and the wider ecosystem to adopt a consistent approach to the use of inclusive language and to eradicate and replace offensive terms. We recognise that this will take time. This course may contain references to non-inclusive language; it will be updated with newer terms as those terms are agreed and ratified with the wider community. We recognise that some of you will be accustomed to using the previous terms and may not immediately recognise their replacements. Please refer to the following examples:
+
+•	When introducing the AMBA 3 AHB-Lite Protocols, we will use the term ‘Manager’ instead of ‘Master’ and ‘Subordinate’ instead of ‘Slave’. 
+ 
+Contact us at education@arm.com with questions or comments about this course. You can also report non-inclusive and offensive terminology usage in Arm content at terms@arm.com.
---
+++ VLSI_Education_Kit/html/js/config.js
@@ -0,0 +1,190 @@
+var index = 
+{
+  "titles": {
+    "title": "Arm Education Kit",
+    "libname": "",
+    "libtype": "VLSI Fundamentals: A Practical Approach"
+  },
+  "headers": [
+    {
+      "name": "Lecture Presentation",
+      "column": "modules"
+    },
+    {
+      "name": "Lab",
+      "column": "exercises"
+    }
+  ],
+  "modules": [
+    [
+      {
+        "column": "modules",
+        "name": "Module 1: Introduction",
+        "link": "contents/Module_1/Module_1_intro.pptx"
+      },
+	  {
+        "column": "exercises",
+        "name": "Getting Started Lab Manual",
+        "link": "contents/Module_1/Lab/Getting_Started.docx"
+      },
+	  {
+        "column": "exercises",
+        "name": "Supporting files and solutions for all labs",
+        "link": "contents/Module_1/Lab/cmosvlsi.tar.gz"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 2: Circuit & Layout",
+        "link": "contents/Module_2/Module_2_cktlay.pptx"
+      },
+	  {
+        "column": "exercises",
+        "name": "Lab Manual",
+        "link": "contents/Module_2/Lab/LAB_1.docx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 3: Simple Processor Example",
+        "link": "contents/Module_3/Module_3_processorEx.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 4: CMOS Transistor Theory",
+        "link": "contents/Module_4/Module_4_transistors.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 5: Non-ideal Transistor Theory ",
+        "link": "contents/Module_5/Module_5_nonideal.pptx"
+      }
+    ],
+    [
+	  {
+        "column": "modules",
+        "name": "Module 6: DC & Transient Response",
+        "link": "contents/Module_6/Module_6_dctran.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 7: Logical Effort",
+        "link": "contents/Module_7/Module_7_logicaleffort.pptx"
+      },
+	  {
+        "column": "exercises",
+        "name": "Lab Manual",
+        "link": "contents/Module_7/Lab/LAB_2.docx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 8: Power",
+        "link": "contents/Module_8/Module_8_power.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 9: Scaling",
+        "link": "contents/Module_9/Module_9_scaling.pptx"
+      }
+    ],
+    [
+	  {
+        "column": "modules",
+        "name": "Module 10: SPICE Simulation",
+        "link": "contents/Module_10/Module_10_spice.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 11: Combinational Circuit Design",
+        "link": "contents/Module_11/Module_11_comb.pptx"
+      },
+	  {
+        "column": "exercises",
+        "name": "Lab Manual",
+        "link": "contents/Module_11/Lab/LAB_3.docx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 12: Sequential Circuit Design",
+        "link": "contents/Module_12/Module_12_seq.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 13: Wires",
+        "link": "contents/Module_13/Module_13_wires.pptx"
+      }
+    ],
+	[
+      {
+        "column": "modules",
+        "name": "Module 14: Adders",
+        "link": "contents/Module_14/Module_14_adders.pptx"
+      }
+    ],
+	[
+      {
+        "column": "modules",
+        "name": "Module 15: Datapath Functional Units",
+        "link": "contents/Module_15/Module_15_datapath.pptx"
+      }
+    ],
+	[
+      {
+        "column": "modules",
+        "name": "Module 16: SRAM",
+        "link": "contents/Module_16/Module_16_sram.pptx"
+      }
+    ],
+	[
+      {
+        "column": "modules",
+        "name": "Module 17: Clocking",
+        "link": "contents/Module_17/Module_17_clocking.pptx"
+      },
+	  {
+        "column": "exercises",
+        "name": "Lab Manual",
+        "link": "contents/Module_17/Lab/LAB_4.docx"
+      }
+    ],
+	[
+      {
+        "column": "modules",
+        "name": "Module 18: Pitfalls",
+        "link": "contents/Module_18/Module_18_pitfalls.pptx"
+      }
+    ],
+	[
+      {
+        "column": "modules",
+        "name": "Module 19: Test",
+        "link": "contents/Module_19/Module_19_test.pptx"
+      }
+    ],
+	[
+      {
+        "column": "modules",
+        "name": "Module 20: Packing, I/O & Power Distribution",
+        "link": "contents/Module_20/Module_20_custom.pptx"
+      }
+    ]
+  ]
+}
